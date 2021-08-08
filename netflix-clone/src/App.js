46a0478503d05f26477e08d28e9588225dcc2c5f
mohamad <msiysinyuy@gmail.com>
2021-08-08 11:16:32 +0100

    the banner and the display of the movie done

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import Row from './components/Row'
+import request from './requests'
+import Banner from './components/Banner'
 
 function App() {
   return (
     <div className="App">
-      this is app page
-      
+    <Banner />
+
+      <Row
+      isLargeRow
+      title="NETFLIX ORIGINALS"
+      fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+
     </div>
   );
 }
