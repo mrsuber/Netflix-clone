b74ab96c54c048b81e1252abe69dd34649084497
mohamad <msiysinyuy@gmail.com>
2021-08-08 08:33:37 +0100

    seting up the axios file

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -1,13 +1,13 @@
 const API_KEY = "79a13da01e3a23333e21513d4d2f3f64";
 
 const requests ={
-  fetchTrending:``,
-  fetchNetflixOriginals:``,
-  fetchTopRated:``,
-  fetchActionMovies:``,
-  fetchComedyMovies:``,
-  fetchHorrorMovies:``,
-  fetchRomanceMovies:``,
-  fetchDocumentaries:``,
+  fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
-export default requests:
+export default requests
